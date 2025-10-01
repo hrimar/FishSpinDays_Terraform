e37b69e2dba6f298b36f51dd140a4eebc996e1b6
Hristo <hhristoff@yahoo.com>
2025-10-01 18:32:55 +0300

    Create SQL Admin and Managed Identity User via pipeline

--- IaC/main.tf
+++ IaC/main.tf
@@ -81,16 +81,16 @@ resource "azurerm_key_vault" "kv" {
 }
 
 # ---- KEY VAULT ACCESS POLICIES ----
-resource "azurerm_key_vault_access_policy" "personal_keyvault_policy" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+# resource "azurerm_key_vault_access_policy" "personal_keyvault_policy" {
+#   key_vault_id = azurerm_key_vault.kv.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = data.azurerm_client_config.current.object_id
 
-  secret_permissions = ["Get", "List", "Set", "Delete", "Purge", "Recover"]
-  key_permissions    = ["Get", "List", "Create", "Delete"]
+#   secret_permissions = ["Get", "List", "Set", "Delete", "Purge", "Recover"]
+#   key_permissions    = ["Get", "List", "Create", "Delete"]
 
-  depends_on = [azurerm_key_vault.kv]
-}
+#   depends_on = [azurerm_key_vault.kv]
+# }
 
 resource "azurerm_key_vault_access_policy" "webapp_keyvault_policy" {
   key_vault_id = azurerm_key_vault.kv.id
