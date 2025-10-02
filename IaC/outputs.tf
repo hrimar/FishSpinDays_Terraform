eb41d7d270ba2cd772ee03141da6605560138a6a
Hristo <hhristoff@yahoo.com>
2025-10-02 17:47:07 +0300

    More outputs & updated pipeline (added SP permission to manipulate SQL before that)

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -1,9 +1,24 @@
+output "resource_group_name" {
+  value       = azurerm_resource_group.rg.name
+  description = "Resource group name"
+}
+
 output "webapp_url" {
   value = azurerm_windows_web_app.webapp.default_hostname
 }
 
 output "sql_server_name" {
   value = azurerm_mssql_server.sql.name
+}
+
+output "sql_server_fqdn" {
+  value       = azurerm_mssql_server.sql.fully_qualified_domain_name
+  description = "SQL Server FQDN"
+}
+
+output "sql_db_name" {
+  value       = azurerm_mssql_database.db.name
+  description = "Database name"
 }
 
 output "key_vault_uri" {
